17628482de55675d04f3e4d210e26fe5e19f9881
jessmitch42 <jessicamitch42@gmail.com>
2018-03-23 17:23:49 -0400

    hand rolling content-- starting

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,36 @@
 # Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 
 # Mary Corse
-artist = Artist.create(first_name:, last_name:, birth_year:, death_year:, nationality:, bio:, sources:)
+artist = Artist.create(first_name: "Mary", last_name: "Corse", birth_year: 1945, nationality: "American", bio: `Best known for her exploration of radiant and interactive surfaces and her innovative technique of painting, Mary Corse first gained recognition for her involvement in the Light and Space movement in 1960s Southern California, along with James Turrell, Robert Irwin, and Douglas Wheeler. Like many of these artists, Corse rejected Abstract Expressionist practices of using accident and relinquishing intention in the application of pigment, favoring instead the controlled formal geometry of Minimalism. As the viewer moves or surrounding light conditions alter, Corse’s works seem to shift subtly, creating a complex spatial and temporal dynamic. As the artist herself eloquently said, “Where there is space, there is time.”`, sources:"https://www.artsy.net/artist/mary-corse"
 
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 
 # Nasreen Mohamedi
-artist = Artist.create(first_name:, last_name:, birth_year:, death_year:, nationality:, bio:, sources:)
+artist = Artist.create(first_name: "Nasreen", last_name: "Mohamedi", birth_year: 1937, death_year:1990, nationality:"Indian", bio:"Nasreen Mohamedi was an Indian artist best known for her line-based drawings, and is today considered one of the most essential modern artists from India. Despite being relatively unknown outside of her native India during her lifetime, Mohamedi's work has been the subject of remarkable revitalisation in international critical circles and has received popular acclaim over the last decade. Her work has been exhibited at the Museum of Modern Art (MoMA) in New York, the Kiran Nadar Museum of Art in New Delhi, documenta in Kassel, Germany, and at Talwar Gallery, which organised the first solo exhibition of her work outside of India in 2003, and which has represented her estate for over a decade, in New York and in New Delhi. Today, Mohamedi is considered one of the major figures of the art of the twentieth century.
+", sources: "https://www.artsy.net/artist/nasreen-mohamedi, https://en.wikipedia.org/wiki/Nasreen_Mohamedi")
 
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 
 # Carmen Herrera
-artist = Artist.create(first_name:, last_name:, birth_year:, death_year:, nationality:, bio:, sources:)
+artist = Artist.create(first_name:"Carmen", last_name:"Herrera", birth_year:1915, nationality:"Cuban", bio:`Carmen Herrera is a Cuban-American abstract, minimalist painter. She was born in Havana and has lived in New York City since the mid-1950s. Herrera's abstract works have brought her international recognition late in life. She turned 100 in May 2015. The key to understanding Carmen Herrera’s style is remembering that, before she left Cuba, she was trained as an architect. This foundation can be seen in her urge to use measurements and tools to create orderly art in a chaotic world. In her own words, Herrera thinks about “the line, the paper, about a lot of tiny things that get bigger and bigger… and then a picture comes up." She is also a self-professed believer in the adage that “less is more”.`, sources:"https://www.artsy.net/artist/carmen-herrera, https://en.wikipedia.org/wiki/Carmen_Herrera")
 
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 
 # Agnes Martin
-artist = Artist.create(first_name:, last_name:, birth_year:, death_year:, nationality:, bio:, sources:)
+artist = Artist.create(first_name:"Agnes", last_name:"Martin", birth_year:1912, death_year:2004, nationality: "Canadian-American", bio:`Agnes Bernice Martin, born in Canada, was an American abstract painter. Her work has been defined as an "essay in discretion on inward-ness and silence". Although she is often considered or referred to as a minimalist, Martin considered herself an abstract expressionist. She was awarded a National Medal of Arts from the National Endowment for the Arts in 1998. Since her first solo exhibition in 1958, Martin’s work has been the subject of more than 85 solo shows and two retrospectives including the survey, Agnes Martin, organized by the Whitney Museum of American Art, New York.`, sources: "https://www.artsy.net/artist/agnes-martin, https://en.wikipedia.org/wiki/Agnes_Martin")
 
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 
 # Anne Truitt
-artist = Artist.create(first_name:, last_name:, birth_year:, death_year:, nationality:, bio:, sources:)
+artist = Artist.create(first_name:"Anne", last_name:"Truitt", birth_year:1921, death_year:2014, nationality:"American", bio:"Anne Truitt was a major American artist of the mid-20th century. She became a full-time artist in the 1950s and made what is considered her most important work in the early 1960s anticipating in many respects the work of minimalists like Donald Judd. She was unlike the minimalists in some significant ways. The sculptures that made her significant to the development of Minimalism were aggressively plain and painted structures, often large. Fabricated from wood and painted with monochromatic layers of acrylic, they often resemble sleek, rectangular columns or pillars. Truitt produces in scale drawings of her structures that are then produced by a cabinetmaker. The structures are weighed to the ground and are often hollow, allowing the wood to breathe in changing temperatures.", sources:``)
 
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
 Artwork.create(artist_id: artist.id, name:, year_completed:, materials:, img:)
