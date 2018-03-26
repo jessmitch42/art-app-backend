66f0c3ae3acec67ac1b62774a819cfa0ae284e8e
jessmitch42 <jessicamitch42@gmail.com>
2018-03-26 15:12:04 -0400

    a couple instance methods for frontend

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,8 @@
 class Artist < ApplicationRecord
   has_many :artworks
+
+  def name
+    `#{self.first_name} #{self.last_name}`
+  end
+
 end
--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -1,3 +1,7 @@
 class Artwork < ApplicationRecord
   belongs_to :artist
+
+  def image_path
+    `/assets/images/#{self.img}`
+  end
 end
