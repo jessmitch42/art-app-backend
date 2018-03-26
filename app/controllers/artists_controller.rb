4ddd7d07b0f0d6638021cb2ded0a854b27d29ae9
jessmitch42 <jessicamitch42@gmail.com>
2018-03-26 15:17:56 -0400

    starting controllers

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,13 @@
+class ArtistsController < ApplicationController
+  def index
+    @artists = Artist.all
+
+    render json: @artists
+  end
+
+  def show
+    @artist = Artist.find(param[:id])
+
+    render json: @artist
+  end
+end
---
+++ app/controllers/artworks_controller.rb
@@ -0,0 +1,13 @@
+class ArtworksController < ApplicationController
+   def index
+     @artworks = Artwork.all
+
+     render json: @artworks
+   end
+
+   def show
+     @artwork = Artwork.find(param[:id])
+
+     render json: @artwork
+   end
+end
