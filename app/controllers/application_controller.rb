cf7a86565703d83e9bb609173618959334bbd12b
jessmitch42 <jessicamitch42@gmail.com>
2018-03-26 14:52:44 -0400

    data in! starting on api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def index
+    @artists = Artist.all
+    @artworks = Artwork.all
+
+    render 'home/index'
+  end
 end
