1a1694fbec0944661d0e0ddf5c7427b97b363f3b
jessmitch42 <jessicamitch42@gmail.com>
2018-03-26 14:53:26 -0400

    removed testing route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def index
-    @artists = Artist.all
-    @artworks = Artwork.all
-
-    render 'home/index'
-  end
 end
