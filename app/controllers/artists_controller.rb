31e7f6dc5fb5e749d8f13433ced108972cd1aaf5
jessmitch42 <jessicamitch42@gmail.com>
2018-03-27 17:14:35 -0400

    just whitespace

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,11 @@
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
-
     render json: @artists
   end
 
   def show
     @artist = Artist.find(param[:id])
-
     render json: @artist
   end
 end
--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,13 +1,11 @@
 class ArtworksController < ApplicationController
    def index
      @artworks = Artwork.all
-
      render json: @artworks
    end
 
    def show
      @artwork = Artwork.find(param[:id])
-
      render json: @artwork
    end
 end
