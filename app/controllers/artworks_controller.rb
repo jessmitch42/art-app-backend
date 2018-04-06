c2bba17c3dd1b759846f9f7601f2ea7600b8c3b0
jessmitch42 <jessicamitch42@gmail.com>
2018-04-05 20:21:37 -0400

    updating artworks controller

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -1,6 +1,6 @@
 class ArtworksController < ApplicationController
    def index
-     @artworks = Artwork.all
+     @artworks = Artwork.find_by(artist_id:param[:id])
      render json: @artworks
    end
 
