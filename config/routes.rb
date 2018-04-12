6e9b9c300a68f379255f95c53ae57dc343084f24
jessmitch42 <jessicamitch42@gmail.com>
2018-04-12 12:21:30 -0400

    restricting routes to ones currently used

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :artists do
-    resources :artworks
+  resources :artists, only: [:index, :show] do
+    resources :artworks, only: [:index, :show]
   end
 end
