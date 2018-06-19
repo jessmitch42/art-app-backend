d30eac4f827f22a077f96a908411854853352a99
jessmitch42 <jessicamitch42@gmail.com>
2018-06-19 11:34:32 -0400

    cors update

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', headers: :any, methods: [:get]
+    end
+  end
 end
